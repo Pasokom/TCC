bbdc6ed5e7a6364c618397ab88395ffa60de71b0
Rafael Reis <42192188+Reis278@users.noreply.github.com>
2018-11-06 18:35:06 -0200

    classes

--- CyberLife_Calendar/src/Login.java
+++ CyberLife_Calendar/src/Login.java
@@ -1,41 +1,58 @@
-
-
-import org.omg.CosNaming._BindingIteratorImplBase;
+import java.time.chrono.MinguoChronology;
+import java.util.Optional;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
-public class Login extends Scene{
+public class Login extends Scene {
 
 	private Label lblTitle;
 	private Label lblEmail;
 	private Label lblSenha;
-	
+
 	private Label lblTitleCadast;
 	private Label lblNomeCadast;
 	private Label lblSobrenomeCadast;
 	private Label lblEmailCadast;
 	private Label lblSenhaCadast;
 	private Label lblSenhaConfirmCadast;
-	
+
 	private Button btnEntrar;
 	private Button btnCadastrar;
-	
+	private Button rdHabilitarCadast;
+	private Button backLogin;
+
 	private TextField txtEmail;
 	private PasswordField pswSenha;
-	
-	
-	
+
+	private TextField txtNomeCadast;
+	private TextField txtSobrenomeCadast;
+	private TextField txtEmailCadast;
+	private PasswordField txtSenhaCadast;
+	private PasswordField txtSenhaConfirmCadast;
+
+	VBox vbLogin = new VBox();
+	VBox vbCadastro = new VBox();
+	HBox hCadastro = new HBox();
+	HBox hLogin = new HBox();
+
+	AnchorPane aPane = new AnchorPane();
+
+	GridPane pnlCadastro = new GridPane();
+
 	public Login() {
 		
 		/** 
@@ -44,25 +61,44 @@ public class Login extends Scene{
 		 */
 		super(new HBox());
 		
-		lblTitleCadast = new Label("Cadastre-se");
-		lblTitleCadast.setFont(new Font(35));
+		this.getStylesheets().add(this.getClass().getResource("css/login-cadastro.css").toExternalForm());
 		
 		lblTitle = new Label("Login");
-		lblTitle.setFont(new Font(45));
+		lblTitle.setFont(new Font(25));
+		
 		lblTitleCadast = new Label("Cadastro");
-		lblTitleCadast.setFont(new Font(45));
+		lblTitleCadast.setFont(new Font(25));
 		
-		lblEmail = new Label("Nome: ");
-		lblSenha = new Label("Senha: ");
-		lblTitle.setFont(new Font(35));
-		
-		lblEmail = new Label("E-mail: ");
+		lblEmail = new Label("E-mail ");
 		txtEmail = new TextField();
+		lblEmailCadast = new Label("E-mail");	
+		txtEmailCadast = new TextField();
+		lblNomeCadast = new Label("Nome");
+		txtNomeCadast = new TextField();
+		lblSobrenomeCadast = new Label("Sobrenome");
+		txtSobrenomeCadast = new TextField();
+		lblSenhaCadast = new Label("Senha: ");
+		txtSenhaCadast = new PasswordField();
+		lblSenhaConfirmCadast = new Label("Confirmar senha");
+		txtSenhaConfirmCadast = new PasswordField();
+		
+		
+		lblSenha = new Label("Senha: ");
+		pswSenha = new PasswordField();
 		
 		lblEmailCadast = new Label("E-mail: ");
 		lblSenhaCadast = new Label("Senha: ");
-
-
+		
+		rdHabilitarCadast = new Button("Cadastrar-se");
+		rdHabilitarCadast.setOnAction(e -> {
+			componenteLogin();
+		});
+		
+		backLogin = new Button("Voltar");
+		backLogin.setOnAction(e -> {
+				componenteLogin();
+		});
+		
 		btnEntrar = new Button("Entrar");
 		btnEntrar.setOnAction(event->{
 			
@@ -70,32 +106,78 @@ public class Login extends Scene{
 		});
 		btnCadastrar = new Button("Cadastrar-se");
 		btnCadastrar.setOnAction(event -> {
-			new Alert(AlertType.CONFIRMATION, "Você foi cadastrado " + txtEmail.getText()).show();
+			Optional<ButtonType> vOptional = new Alert(AlertType.CONFIRMATION, "Você foi cadastrado " + txtNomeCadast.getText() + " " + txtSobrenomeCadast.getText()).showAndWait();
 
+			if(vOptional.get() == ButtonType.OK){
+				componenteLogin();
+			}
 		});  
 		
 		GridPane pnlLayout = new GridPane();
-		
-	    AnchorPane aPane = new AnchorPane();
 	    
-	    AnchorPane.setRightAnchor(pnlLayout, 50d);
-	    AnchorPane.setTopAnchor(pnlLayout, 20d);
+	    AnchorPane.setRightAnchor(pnlLayout, 5d);
+	    AnchorPane.setTopAnchor(pnlLayout, 5d);
+	    
+		pnlCadastro.add(lblTitleCadast, 0, 6, 2, 1);	
+		pnlCadastro.add(lblNomeCadast, 0, 7, 2, 1);
+		pnlCadastro.add(txtNomeCadast, 0, 8, 2, 1);
+		pnlCadastro.add(lblSobrenomeCadast, 0, 9, 2, 1);
+		pnlCadastro.add(txtSobrenomeCadast, 0, 10, 2, 1);
+		pnlCadastro.add(lblEmailCadast, 0, 11, 2, 1);
+		pnlCadastro.add(txtEmailCadast, 0, 12, 2, 1);
+		pnlCadastro.add(lblSenhaCadast, 0, 13, 2, 1);
+		pnlCadastro.add(txtSenhaCadast, 0, 14, 2, 1);
+		pnlCadastro.add(lblSenhaConfirmCadast, 0, 15, 2, 1);
+		pnlCadastro.add(txtSenhaConfirmCadast, 0, 16, 2, 1);
+		pnlCadastro.add(btnCadastrar, 0, 17, 2, 2);
 		
-		pnlLayout.setPadding(new Insets(180));
+		vbLogin.getChildren().addAll(lblTitle, lblEmail, txtEmail, lblSenha, pswSenha, hLogin);
+		vbCadastro.getChildren().addAll(lblTitleCadast, lblNomeCadast, txtNomeCadast, lblSobrenomeCadast, txtSobrenomeCadast, lblEmailCadast, txtEmailCadast, lblSenhaCadast, txtSenhaCadast, lblSenhaConfirmCadast, txtSenhaConfirmCadast, hCadastro);
+			    
+		hCadastro.getChildren().addAll(backLogin, btnCadastrar);
+		hCadastro.setAlignment(Pos.CENTER_LEFT);
+		hCadastro.setSpacing(10);
+		vbCadastro.setAlignment(Pos.CENTER_LEFT);
+		
+		hLogin.getChildren().addAll(btnEntrar, rdHabilitarCadast);
+		hLogin.setAlignment(Pos.CENTER_LEFT);
+		hLogin.setSpacing(10);
+		vbLogin.setAlignment(Pos.CENTER_LEFT);
 
-		pnlLayout.setVgap(10);
-		pnlLayout.setHgap(10);
+		aPane.getChildren().add(vbLogin);
+		vbLogin.setSpacing(10);
+		vbLogin.setPadding(new Insets(25,100,25,25));
+		vbCadastro.setSpacing(7);
+		vbCadastro.setPadding(new Insets(25,100,25,25));
 		
-		pnlLayout.add(lblTitleCadast, 0, 4, 2, 1);	
-		pnlLayout.add(lblTitle, 0, 0, 2, 1);
+
+		AnchorPane.setTopAnchor(vbLogin, 0d);
+		AnchorPane.setBottomAnchor(vbLogin, 0d);
+		AnchorPane.setRightAnchor(vbLogin, 0d);
 		
-		pnlLayout.add(lblEmail, 0, 1);
-		pnlLayout.add(txtEmail, 0, 2);
-	    
-	    aPane.getChildren().add(pnlLayout);
+		AnchorPane.setTopAnchor(vbCadastro, 0d);
+		AnchorPane.setBottomAnchor(vbCadastro, 0d);
+		AnchorPane.setRightAnchor(vbCadastro, 0d);
 		
 		this.setRoot(aPane);
 		
+	}
+
+	private void componenteLogin() {
+		
+		if ( aPane.getChildren().size() > 0) { 
+			
+			boolean x  = aPane.getChildren().get(0) == vbCadastro;
+			
+			if (! x ) { 
+				aPane.getChildren().set(0, vbCadastro);
+				
+			}else {
+				aPane.getChildren().set(0, vbLogin);
+			}
+		}
+		
+		Main.main_stage.sizeToScene();
 	}
 
 }
--- CyberLife_Calendar/src/Main.java
+++ CyberLife_Calendar/src/Main.java
@@ -1,4 +1,3 @@
-import db.Database;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -13,24 +12,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		Database.get_connection();
+//		Database.get_connection();
 		
 		
 		// db.queryTeste(); // fazendo testes
 
 		main_stage = primaryStage;
 
-		main_stage.setScene(new Reminder());
+		main_stage.setScene(new Login());
 		
 		main_stage.show();
-<<<<<<< HEAD
-=======
-//		janela = primaryStage;
-//
-//		main_stage.setScene(new HomePage());
-//		
-//		main_stage.show();		
->>>>>>> b8b7cfc21f2f8bd23532b33e917612b0ba7bddd1
 
 	}
 }
