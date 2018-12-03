8af7e54c5e41e2c2bb911b19aa50ed0888dd3e57
Manoel Almeida <tmanoel64x@gmail.com>
2018-12-03 11:25:10 -0200

    app em segundo plano sem gastar 400mb de ram

--- CyberLife_Calendar/src/statics/NotifyUser.java
+++ CyberLife_Calendar/src/statics/NotifyUser.java
@@ -8,6 +8,7 @@ import java.awt.SystemTray;
 import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.TrayIcon.MessageType;
+import java.util.Calendar;
 
 public class NotifyUser {
 
@@ -41,10 +42,36 @@ public class NotifyUser {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		verify();
 	}
 	
-	public static void verify() {
+	private static void verify() {
+		
+		Thread thread = new Thread(new Runnable() {
 			
+			@Override
+			public void run() {
+				
+				int minute = -1;
+				
+				while(true) {
+					
+					if(minute != Calendar.getInstance().get(Calendar.MINUTE)) {
+						
+						sendNotification("Importante", "passou um minuto", MessageType.ERROR);
+						minute = Calendar.getInstance().get(Calendar.MINUTE);
+						try {
+							Thread.sleep(1000 * (60 - Calendar.getInstance().get(Calendar.SECOND)));
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+		
+		thread.start();
 	}
 	
 	public static void sendNotification(String caption, String text, MessageType messageType) {
