469169f77cb8e0b5e9cc543df26bd5af60765fd9
Rafael Reis <rafaelreis278@gmail.com>
2018-12-07 10:03:37 -0200

    notificação dos eventos

--- CyberLife_Calendar/src/statics/NotifyUser.java
+++ CyberLife_Calendar/src/statics/NotifyUser.java
@@ -8,7 +8,15 @@ import java.awt.SystemTray;
 import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.TrayIcon.MessageType;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+
+import javax.naming.event.EventContext;
+
+import db.functions.event.RetrieveEvents;
+import db.pojo.eventPOJO.EventDB;
 
 public class NotifyUser {
 
@@ -58,9 +66,18 @@ public class NotifyUser {
 				while(true) {
 					
 					if(minute != Calendar.getInstance().get(Calendar.MINUTE)) {
-						
-						sendNotification("Teste", "oi", MessageType.NONE);
 
+						RetrieveEvents retrieveEvents = new RetrieveEvents();
+						ArrayList<EventDB> lista = retrieveEvents.getNowEvents();
+
+						Format formatter = new SimpleDateFormat("HH:mm");
+						
+						for (EventDB event : lista) {
+							String hora = formatter.format(event.getData_inicio());
+
+							sendNotification("Evento", event.getTitulo() + " " + hora, MessageType.NONE);
+						}
+						
 						minute = Calendar.getInstance().get(Calendar.MINUTE);
 						try {
 							Thread.sleep(1000 * (60 - Calendar.getInstance().get(Calendar.SECOND)));
