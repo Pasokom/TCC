ed9a9f2185f3fe88d4a1bb0b66f14f5ca604adb5
Reis278 <rafaelreis278@gmail.com>
2019-06-11 01:00:13 -0300

    check notififcation user for projects

--- CyberLife_Calendar/src/component/NotificationComponent.java
+++ CyberLife_Calendar/src/component/NotificationComponent.java
@@ -1,11 +1,18 @@
 package component;
 
+import java.awt.Image;
+
+import db.functions.registrationAndLogin.HandlerLogin;
 import db.functions.user.ManageNotifications;
 import db.pojo.NotificationDB;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import statics.SESSION;
 
 public class NotificationComponent extends HBox {
 
@@ -13,13 +20,43 @@ public class NotificationComponent extends HBox {
     private Button btn_accept;
     private Button btn_dismiss;
 
+    private Circle profileImg;
+    private StackPane userImg;
+    private Label userInitial;
+    private Image img;
+
     public NotificationComponent(NotificationDB notification) {
         
         text = new Label(notification.getTitle());
         text.setWrapText(true);
 
-        btn_accept = new Button("Aceitar");
-        btn_accept.setPrefWidth(160);
+        this.getStylesheets().add(this.getClass().getResource("../css/notificationComponent.css").toExternalForm());
+
+        /*profileImg = new Circle();
+        profileImg.setRadius(10);
+		profileImg.setCenterX(100);
+        profileImg.setCenterY(100);
+        
+        userImg = new StackPane();
+
+        HandlerLogin handlerLogin = new HandlerLogin();
+
+		if (handlerLogin.userImageExists()){
+
+			img = new Image("http://localhost/cyberlife/imagens/img" + notification.getUser_cod() + ".jpeg");
+			profileImg.setFill(new ImagePattern(img));
+			userImg.getChildren().addAll(profileImg);
+		}
+		else {
+
+			userInitial = new Label(notification.getTitle().substring(0, 1).toUpperCase());
+			userInitial.setFont(new Font(20));
+			userImg.getChildren().addAll(profileImg, userInitial);
+		}
+        */
+
+        btn_accept = new Button();
+        btn_accept.setId("btnDone");    
 
         btn_accept.setOnAction(e -> {
 
@@ -28,9 +65,9 @@ public class NotificationComponent extends HBox {
             manage.accept(notification);
         });
 
-        btn_dismiss = new Button("Recusar");
-        btn_dismiss.setPrefWidth(160);
-        
+        btn_dismiss = new Button();
+        btn_dismiss.setId("btnCancel");
+
         btn_dismiss.setOnAction(e -> {
 
             this.getScene().getWindow().hide();
